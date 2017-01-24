e22474ffe4290d6691f9f0422ae1c6fbec9970ab
James Dabbs <jamesdabbs@gmail.com>
2017-01-24 12:04:24 -0500

    Add create action

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,4 +1,13 @@
 class IssuesController < ApplicationController
   def new
   end
+
+  def create
+    Issue.create(
+        title:       'Title',
+        description: 'Desc'
+    )
+
+    redirect_to '/'
+  end
 end
