83415784dca2ab71850288670ab4d69d817bbcb1
James Dabbs <jamesdabbs@gmail.com>
2017-01-25 11:36:47 -0500

    Only allow selecting from active developers

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,4 +1,12 @@
 class Developer < ActiveRecord::Base
+  # def self.retired
+  #   all.select { |dev| dev.retired? }
+  # end
+  # def Developer.active
+  def self.active
+    where(retired_at: nil)
+  end
+
   def retire
     update! retired_at: Time.now
   end
